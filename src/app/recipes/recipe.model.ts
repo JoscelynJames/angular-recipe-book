d275c163b3fe4d3684cb206610ccfd35da9f711e
Joscelyn James <josce.james7@hotmail.com>
2018-02-26 15:26:08 -0700

    completed shopping-list service

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,15 @@
-export default class Recipe {
-	constructor(public name: string, public description: string, public imagePath: string) {}
+import Ingredient from '../shared/ingredient.model';
+
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+  public ingredients: Ingredient[];
+
+  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,40 @@
-import { EventEmitter } from '@angular/core';
-import Recipe from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import Ingredient from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 	
 	private recipes: Recipe[] = [
-    new Recipe('Pizza', 'The best thing in the world', 'https://www.cicis.com/media/1137/pizza_trad_alfredo.png'),
-    new Recipe('Pepperoni Pizza', 'The best thing in the world', 'https://www.cicis.com/media/1138/pizza_trad_pepperoni.png')
+    new Recipe(
+			'Pizza', 
+			'The best thing in the world', 
+			'https://www.cicis.com/media/1137/pizza_trad_alfredo.png',
+			[
+				new Ingredient('Dough', 1),
+				new Ingredient('Red Sauce', 1),
+				new Ingredient('Cheese', 10),
+			]),
+    new Recipe(
+			'Pepperoni Pizza', 
+			'The best thing in the world', 
+			'https://www.cicis.com/media/1138/pizza_trad_pepperoni.png',
+			[
+				new Ingredient('Dough', 1),
+				new Ingredient('Red Sauce', 1),
+				new Ingredient('Cheese', 10),
+				new Ingredient('Pepperoni', 15),
+			])
   ];
+	constructor(private slService: ShoppingListService) { }
 
 	getRecipes() {
 		return this.recipes.slice();
+	}
+
+	addIngredientToShoppingList(ingredients: Ingredient[]) {
+		this.slService.addIngredients(ingredients);
 	}
 }
